80a2d3a6b6a085c9d4de7c9324fd617a9eb9fbe5
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-10-29 16:19:45 +0800

    Lesson 3 JavaScript

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// variable = A container that stores a value.
-//            Behaves as if it were the value it contains.
-
-let fullName = "John Vincent P. Espinosa";
-let age = 22;
-let isStudent = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
