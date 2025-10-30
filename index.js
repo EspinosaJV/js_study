27210951fe55c44dd9d67eb42847f07037861a5d
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-10-30 09:40:57 +0800

    Lesson 4 JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-// arithmetic operators = operands (values, variables, etc.)
-//                        operators (+ - * /)
-//                        ex. 11 = x + 5;
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myH1").textContent = `Hello ${username}`;
+}
