c0aa5f3519fdd1e098705eaafbec7be8f4f94a32
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-07 17:22:28 +0800

    Lesson 6 JavaScript

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// type conversion = change the datatype of a value to another
-//                   (strings, numbers, booleans)
+// const = a variable that can't be changed
 
-let x;
-let y;
-let z;
+const PI = 3.14159;
+let radius;
+let circumference;
 
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + " cm";
+}
