e05d3e0257c6ee4b69ed4b90ad4e13422da12b97
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-12 15:36:00 +0800

    JavaScript Lesson 9

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-// Math = built-in object that provides
-//        a collection of properties and methods
+// RANDOM NUMBER GENERATOR
 
-let x = 3;
-let y = 2;
-let z = 1;
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-//z = Math.round(x);
-//z = Math.floor(x);
-//z = Math.ceil(x);
-//z = Math.trunc(x);
-//z = Math.pow(y, x);
-//z = Math.sqrt(x);
-//z = Math.log(x);
-//z = Math.sin(x);
-//z = Math.cos(x);
-//z = Math.tan(x);
-//z = Math.abs(x);
-//z = Math.sign(x);
-let max = Math.max(x, y, z);
-let min = Math.min(x, y, z);
-
-
-console.log(max);
-console.log(min);
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
