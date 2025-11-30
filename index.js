a3a557379b73c0b87880339563a75e020734008c
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-01 00:35:28 +0800

    Mobile Portrait All Of Your Bombs Placeholder Templates
    JS Implementation of placeholder templates for movie reviews in the mobile portrait view of the "All Of Your Bombs" section
    
    - JV

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-// NUMBER GUESSING GAME
+// function = A section of reusable code.
+//            Declare code once, use it whenever you want.
+//            Call the function to execute that code.
 
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-
-let attempts = 0;
-let guess;
-let running = true;
-
-while(running){
-
-    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-    guess = Number(guess);
-
-    if(isNaN(guess)){
-        window.alert("Please enter a valid number");
-    } else if(guess < minNum || guess > maxNum){
-        window.alert("Please enter a valid number");
-    } else {
-        attempts++;
-        if(guess < answer){
-            window.alert("TOO LOW! TRY AGAIN");
-        } else if (guess > answer){
-            window.alert("TOO HIGH! TRY AGAIN");
-        } else {
-            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts.`)
-            running = false;
-        }
-    }
+function add(x, y){
+    return x + y;
+}
+function subtract(x, y){
+    return x - y;
+}
+function multiply(x, y){
+    return x * y;
+}
+function divide(x, y){
+    return x / y;
+}
+function isEven(){
+    
 }
