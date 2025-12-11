5b3638b5a8666fe190a4f21445c03cdf8e58d752
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-11 17:32:53 +0800

    JavaScript Lesson 27

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-// spread operator = ... allows an iterable such as an
-//                       array or string to be expanded
-//                       into seperate elements
-//                       (unpacks the elements)
+// rest parameters = (...rest) allow a function work with a variable
+//                   number of arguments by bundling them into an array
+//                   spread = expands an array into separate elements
+//                   rest = bundles seperate elements into an array
 
-let numbers = [1, 2, 3, 4, 5];
-let maximum = Math.max(...numbers);
-let minimum = Math.min(...numbers);
+function combineStrings(...strings){
+    return strings.join(" ");
+}
 
-console.log(maximum);
-console.log(minimum);
+const fullName = combineStrings("Mr.", "John Vincent", "Espinosa", "III");
 
-let username = "John Vincent Espinosa";
-let letters = [...username].join("-");
-
-console.log(letters);
-
-let fruits = ["apple", "orange", "banana"];
-let vegetables = ["carrots", "celery", "potatoes"]
-let newFruits = [...fruits];
-let foods = [...fruits, ...vegetables, "eggs", "milk"];
-
-console.log(fruits);
-console.log(newFruits);
-console.log(foods);
+console.log(fullName);
