4445b6dfd496d95d51c6ffed8d0d6152719e1eee
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-16 13:12:38 +0800

    JavaScript ForEach method

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-// callback = a function that is passed as an argument to another function
+// forEach() = method used to iterate over the elements
+//             of an array and apply a specified function (callback)
+//             to each element
 
-sum(displayPage, 1, 2);
+//             array.forEach(callback)
+//             element, index, array are provided
 
-function sum(callback, x, y){
-    let result = x + y;
-    callback(result);
+let fruits = ["APPLE", "ORANGE", "BANANA", "COCONUT"];
+
+fruits.forEach(upperCase);
+fruits.forEach(lowerCase);
+fruits.forEach(capitalize);
+fruits.forEach(display);
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
 }
 
-function displayConsole(result){
-    console.log(result);
+function lowerCase(element, index, array){
+    array[index] = element.toLowerCase();
 }
 
-function displayPage(result){
-    document.getElementById("myH1").textContent = result;
+function capitalize(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+function display(element){
+    console.log(element);
 }
