1150c8546638b33e5b00628ba2e6c2c74c55484b
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-17 18:48:12 +0800

    JavaScript Arrow Functions

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// function expressions = a way to define functions as values or variables
+// arrow functions = a concise way to write function expressions
+//                   good for simple functions that you use only once
+//                   (parameters) => some code
 
-// 1. Callbacks in asynchronous operations
-// 2. Higher-Order Functions
-// 3. Closures
-// 4. Event Listeners
+const numbers = [1, 2, 3, 4, 5, 6];
 
-const hello = function(){
-    console.log("Hello");
-}
+const squares = numbers.map((element) => Math.pow(element, 2));
+const cubes = numbers.map((element) => Math.pow(element, 3));
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 2 !== 0);
+const total = numbers.reduce((accumulator, element) => accumulator + element);
 
-setTimeout(function(){
-    console.log("Goodbye");
-}, 5000);
+console.log(squares);
+console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
+console.log(total);
