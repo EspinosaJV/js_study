ec1413453e568e3f6bd9d08365c43c79c7f90f20
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-17 19:03:55 +0800

    JavaScript This Keyword

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-// object = a collection of related properties and/or methods
-//          can represent real world objects (people, products, places)
-//          object = {key:value,
-//                    function()}
+// this = reference to the object where THIS is used
+//        (the object depends on the immediate context)
+//        person.name = this.name
 
 const person1 = {
-    firstName: "Spongebob",
-    lastName: "Squarepants",
-    age: 30,
-    isEmployed: true,
-    sayHello: () => console.log("Hey! I'm Spongebob!"),
-    eat: function(){ console.log("I am eating a Krabby Patty") },
+    name: "Spongebob",
+    favFood: "Hamburgers",
+    sayHello: function(){ console.log(`Hi! I am ${this.name} and my favorie food is ${this.favFood}`)},
+    eat: function(){ console.log(`${this.name} is eating ${this.favFood}`)},
 }
 
 const person2 = {
-    firstName: "Patrick",
-    lastName: "Star",
-    age: 42,
-    isEmployed: false,
-    sayHello: () => console.log("Hey... I'm Patrick..."),
-    eat: () => console.log("I am eating roast beef, chicken, and pizza"),
-}   
+    name: "Patrick",
+    favFood: "Pizza",
+    sayHello: function(){ console.log(`Hi! I am ${this.name} and my favorite food is ${this.favFood}`)},
+    eat: function(){ console.log(`${this.name} is eating ${this.favFood}`)},
+}
 
-console.log(person1.firstName);
-console.log(person1.lastName);
-console.log(person1.age);
-console.log(person1.isEmployed);
 person1.sayHello();
 person1.eat();
 
-console.log(person2.firstName);
-console.log(person2.lastName);
-console.log(person2.age);
-console.log(person2.isEmployed);
 person2.sayHello();
 person2.eat();
